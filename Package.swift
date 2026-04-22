75636ed210c2790b709deb39166c712c33fe8ab4
Magela Santana <magela.santana@lyra-network.com>
2026-04-22 17:23:06 +0200

    chore: go to V1

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 6.2
+import PackageDescription
+
+let package = Package(
+    name: "LyraCardScan",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "LyraCardScan",
+            targets: ["LyraCardScanTarget"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "LyraCardScanLibrary",
+            url: "https://raw.githubusercontent.com/lyra/card-scan-ios/1.0.0/LyraCardScan.xcframework.zip",
+            checksum: "364da7b51318139907d33574804620e2861d38d92bd2096592d1f25d455600c5"
+        ),
+        .target(
+            name: "LyraCardScanTarget",
+            dependencies: [
+                "LyraCardScanLibrary"
+            ]
+        )
+    ]
+)
