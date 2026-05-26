7dafddd93f99556aff90b707bdb8697b7a10c287
Magela Santana <magela.santana@lyra-network.com>
2026-05-26 15:04:03 +0200

    fix framework for carthage integration

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "LyraCardScanLibrary",
             url: "https://raw.githubusercontent.com/lyra/card-scan-ios/1.0.0/LyraCardScan.xcframework.zip",
-            checksum: "3013bfc98bff7098fdafeeb4000bef43637ad8385bd42cc3f625a44f21e50158"
+            checksum: "23d9c8e4e17f4a6dd4e2c019be287e35a78327167fd3aa597d6c3473ef76b1b0"
         ),
         .target(
             name: "LyraCardScanTarget",
